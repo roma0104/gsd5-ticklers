9020c31b0aa7e8067556d529eef1249a0326be13
Roma Hicks <roma0104@gmail.com>
2015-05-13 16:28:25 -0500

    Fixed fumbled scripts.

--- bin/build.sh
+++ bin/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 GSD5TICK_DIR=`pwd`
-GSD5_DIR=$GSD5TICK_DIR/../gsd5
-TIDDLYWIKI5_DIR=$GSD5_DIR/../TiddlyWiki5
+TIDDLYWIKI5_DIR=$GSD5TICK_DIR/../TiddlyWiki5
 
 OUTPUT_DIR=$GSD5TICK_DIR/output
-OUTPUT_FILE=gsd5-empty.html
+OUTPUT_FILE=gsd5-ticklers.html
 mkdir -p $OUTPUT_DIR
 
-export TIDDLYWIKI_PLUGIN_PATH=$GSD5_DIR/plugins
+export TIDDLYWIKI_PLUGIN_PATH=$GSD5TICK_DIR/plugins
 
 ( cd $TIDDLYWIKI5_DIR && node ./tiddlywiki.js \
-	$GSD5TICK_DIR/editions/gsd5-ticklers \
+	$GSD5TICK_DIR/editions/gsd5 \
 	--verbose \
 	--output $GSD5TICK_DIR/output \
 	--rendertiddler $:/core/save/all $OUTPUT_FILE text/plain )
