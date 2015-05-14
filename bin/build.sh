b9607ea3ad6577c5a12534b262066fe7dcc400d4
Roma Hicks <roma0104@gmail.com>
2015-05-14 10:50:01 -0500

    Fixed build script.

--- bin/build.sh
+++ bin/build.sh
@@ -10,7 +10,7 @@ mkdir -p $OUTPUT_DIR
 export TIDDLYWIKI_PLUGIN_PATH=$GSD5TICK_DIR/plugins
 
 ( cd $TIDDLYWIKI5_DIR && node ./tiddlywiki.js \
-	$GSD5TICK_DIR/editions/gsd5 \
+	$GSD5TICK_DIR/editions/gsd5-ticklers \
 	--verbose \
 	--output $GSD5TICK_DIR/output \
 	--rendertiddler $:/core/save/all $OUTPUT_FILE text/plain )
